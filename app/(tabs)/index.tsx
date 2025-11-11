add975e40a415734bf0543f31b23bd11ae7098f0
eybaj380 <esha.yasmin.bajwa@gmail.com>
2025-11-11 03:01:59 -0500

    [NOV-11] adding custom font and laying out dashboard page - input form WIP

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,13 @@ export default function HomeScreen() {
   return (
       <View style={styles.container}>
         <Image source={UserAvatar} style={styles.avatarContainer}/>
-        <Text>This is a test</Text>
+        <View style={styles.inputForm}>
+          <Text style={styles.title}>Hello, User!</Text>
+          <View>
+            <Text>Hourly wage</Text>
+            <Text>Enter a dollar amount...</Text>
+          </View>
+        </View>
       </View>
   );
 }
@@ -14,11 +20,24 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
  container: {
   flex: 1,
+  flexDirection: 'row',
+  marginTop: 15,
   marginLeft: 35,
   marginRight: 50
  },
- avatarContainer: {
+  avatarContainer: {
   width: 112,
   height: 110.78,
+  marginRight: 35,
+ },
+ title: {
+  color: '#BE5EAC',
+  fontSize: 32,
+  fontWeight: 'heavy',
+  fontFamily: 'AlikeAngular-Regular',
+ },
+ inputForm: {
+  flexDirection: 'column',
+  gap: 22
  }
 });
