cb4f3db219db890c82fd754bdcbac7dbd61d6369
eybaj380 <esha.yasmin.bajwa@gmail.com>
2025-11-14 13:18:37 -0500

    [NOV-14] saving frontend changes, updating custom input field on dashboard page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,60 @@
 import { Image } from 'expo-image';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import UserAvatar from '../../assets/images/user-avatar.svg';
+import { InputField } from '../../components/input-field';
 
 export default function HomeScreen() {
+  const [income, setIncome] = useState('');
+  const [expenses, setExpenses] = useState('');
+  //change to be an array of strings
+  const [expenseLabel, setExpenseLabel] = useState('');
+
+  const handleSave = () => {
+    console.log('Income: ', income);
+    console.log('Expense recorded: ', expenses);
+    console.log('Description: ', expenseLabel);
+
+    alert(`Data saved: \nIncome - ${income}\nExpenses - ${expenses}\nDescription - ${expenseLabel}`); 
+
+    setIncome('');
+    setExpenses('');
+  };
+
   return (
       <View style={styles.container}>
         <Image source={UserAvatar} style={styles.avatarContainer}/>
         <View style={styles.inputForm}>
           <Text style={styles.title}>Hello, User!</Text>
           <View>
-            <Text>Hourly wage</Text>
-            <Text>Enter a dollar amount...</Text>
+            <InputField 
+              label="Hourly Wage"
+              placeholder="Enter a dollar amount..."
+              value={income}
+              onChangeText={setIncome}
+            />
+             <InputField 
+              label="Salary"
+              placeholder="Enter a dollar amount..."
+              value={income}
+              onChangeText={setIncome}
+            /> 
+            {/* only accept either hourly wage OR salary for income */}
+            {/* need to change form types to only accept currency type */}
+             <InputField 
+              label="Expense"
+              placeholder="Enter a dollar amount..."
+              value={expenses}
+              onChangeText={setExpenses}
+            />
+             <InputField 
+              placeholder="Description..."
+              value={expenseLabel}
+              onChangeText={setExpenseLabel}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button title='Submit' onPress={handleSave} />
           </View>
         </View>
       </View>
@@ -32,12 +76,19 @@ const styles = StyleSheet.create({
  },
  title: {
   color: '#BE5EAC',
-  fontSize: 32,
-  fontWeight: 'heavy',
+  fontSize: 40,
+  fontWeight: '500',
   fontFamily: 'AlikeAngular-Regular',
  },
  inputForm: {
   flexDirection: 'column',
   gap: 22
+ },
+ buttonContainer: {
+  marginTop: 23
+ },
+ submitButton: {
+  borderRadius: 22.14,
+  backgroundColor: '#BE5EAC',
  }
 });
--- components/input-field.tsx
+++ components/input-field.tsx
@@ -1,19 +1,48 @@
-import { useState } from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
+import { StyleProp, StyleSheet, Text, TextInput, TextStyle, View } from 'react-native';
 
-export const InputField = () => {
-      const [inputValue, setInputValue] = useState('');
+type InputFieldProps = {
+  label?: string;
+  placeholder: string;
+  value: string;
+  onChangeText: Dispatch<SetStateAction<string>>;
+  style?: StyleProp<TextStyle>;
+};
 
-      const handleChange = (event: any) => {
-        setInputValue(event.target.value);
-      };
-      
+export const InputField = ({ label, placeholder, value, onChangeText, style, ...restProps}: InputFieldProps) => {
       return (
-        <form>
-          <label>
-            Enter text:
-            <input type="text" value={inputValue} onChange={handleChange} />
-          </label>
-          <p>Current input value: {inputValue}</p>
-        </form>
+        <View style={styles.container}>
+          {label && <Text style={styles.label}>{label}</Text>}
+          <TextInput
+            style={[styles.input, style]}
+            placeholder={placeholder}
+            onChangeText={onChangeText}
+            {...restProps} //Pass through any other native TextInput props
+          />
+        </View>
       );
 };
+
+ const styles = StyleSheet.create({
+  container: {
+    marginBottom: 15,
+  },
+  label: {
+    fontFamily: 'Amethysta-Regular',
+    fontSize: 20,
+    marginBottom: 8,
+    color: '#7D28A4',
+    fontWeight: '600'
+  },
+  input: {
+    height: 40,
+    fontSize: 20,
+    color: '#9a9a9aff',
+    borderColor: '#ccc',
+    fontFamily: 'Amethysta-Regular',
+    fontWeight: 'semibold',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+  },
+ });
