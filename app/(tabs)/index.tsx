7d299d0725c9128acf0a7851c535f55b51705290
eybaj380 <esha.yasmin.bajwa@gmail.com>
2025-11-24 11:46:19 -0500

    [NOV-24] making progress on dashboard: add expense button WIP, welcome message & dismissal, financial report WIP

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { Image } from 'expo-image';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import HandWave from '../../assets/images/hand_wave.svg';
 import UserAvatar from '../../assets/images/user-avatar.svg';
 import { InputField } from '../../components/input-field';
 
@@ -9,6 +10,8 @@ export default function HomeScreen() {
   const [expenses, setExpenses] = useState('');
   //change to be an array of strings
   const [expenseLabel, setExpenseLabel] = useState('');
+  const [isMessageDismissed, setIsMessageDismissed] = useState(false);
+  const [isInputSaved, setIsInputSaved] = useState(false);
 
   const handleSave = () => {
     console.log('Income: ', income);
@@ -26,14 +29,14 @@ export default function HomeScreen() {
         <Image source={UserAvatar} style={styles.avatarContainer}/>
         <View style={styles.inputForm}>
           <Text style={styles.title}>Hello, User!</Text>
-          <View>
+          <ScrollView>
             <InputField 
               label="Hourly Wage"
               placeholder="Enter a dollar amount..."
               value={income}
               onChangeText={setIncome}
             />
-             <InputField 
+            <InputField 
               label="Salary"
               placeholder="Enter a dollar amount..."
               value={income}
@@ -41,20 +44,40 @@ export default function HomeScreen() {
             /> 
             {/* only accept either hourly wage OR salary for income */}
             {/* need to change form types to only accept currency type */}
-             <InputField 
-              label="Expense"
-              placeholder="Enter a dollar amount..."
-              value={expenses}
-              onChangeText={setExpenses}
-            />
-             <InputField 
-              placeholder="Description..."
-              value={expenseLabel}
-              onChangeText={setExpenseLabel}
-            />
-          </View>
-          <View style={styles.buttonContainer}>
-            <Button title='Submit' onPress={handleSave} />
+            <View>
+              <InputField 
+                label="Expense"
+                isButtonPresent={true}
+                placeholder="Enter a dollar amount..."
+                value={expenses}
+                onChangeText={setExpenses}
+              />
+              <InputField 
+                placeholder="Description..."
+                value={expenseLabel}
+                onChangeText={setExpenseLabel}
+              />
+            </View>
+            <Pressable style={styles.buttonContainer} onPress={handleSave}>
+              <Text style={styles.buttonText}>Submit</Text>
+            </Pressable>
+          </ScrollView>
+        </View>
+        <View style={styles.messagesContainer}>
+          {!isMessageDismissed ? (
+            <View style={styles.welcomeContainer}>
+              <Image source={HandWave} style={styles.handWaveContainer}/>
+              <View style={styles.internalTextContainer}>
+                <Text style={styles.welcomeMessage}>Welcome to your personal financial dashboard! To get started, enter your hourly wage or yearly salary as well as adding monthly expenses. Click the plus button to continue adding expenses. Once you have filled out the form, press the Submit button to continue.</Text>
+                <Pressable onPress={() => setIsMessageDismissed(true)}>
+                  <Text style={styles.dismissText}>Dismiss message</Text>
+                </Pressable>
+              </View>
+            </View> 
+          ) : null}
+          <View style={styles.financeReportContainer}>
+            <Text style={[styles.title, {textAlign: 'center'}]}>Financial Report</Text>
+            {!isInputSaved ? (<Text style={styles.noData}>No financial goals set or data given. Please submit income and expenses to generate the report.</Text>) : null}
           </View>
         </View>
       </View>
@@ -79,16 +102,73 @@ const styles = StyleSheet.create({
   fontSize: 40,
   fontWeight: '500',
   fontFamily: 'AlikeAngular-Regular',
+  marginBottom: 24,
  },
  inputForm: {
   flexDirection: 'column',
-  gap: 22
+  marginRight: 30,
  },
  buttonContainer: {
-  marginTop: 23
+  borderRadius: 30,
+  backgroundColor: '#7D28A4',
+  paddingVertical: 10,
  },
- submitButton: {
-  borderRadius: 22.14,
-  backgroundColor: '#BE5EAC',
+ buttonText: {
+   fontFamily: 'Amethysta-Regular',
+   color: '#F5F5F5',
+   fontSize: 25,
+   alignSelf: 'center'
+ },
+ messagesContainer: {
+  flex: 1, 
+  flexDirection: 'column', 
+  gap: 20,
+  paddingBottom: 25
+ },
+ welcomeContainer: {
+  flexDirection: 'row',
+  width: 450,
+  height: 150,
+  backgroundColor: '#D0FFF5',
+  borderRadius: 20,
+  paddingLeft: 15,
+  paddingTop: 10,
+  paddingRight: 20,
+ },
+ internalTextContainer: {
+  flex: 1, 
+  gap: 10
+ },
+ handWaveContainer: {
+  width: 37,
+  height: 37,
+  marginRight: 15,
+ },
+  welcomeMessage: {
+  fontFamily: 'Amethysta-Regular', 
+  fontSize: 16,
+  color: '#757575'
+ },
+ dismissText: {
+  fontFamily: 'Amethysta-Regular', 
+  fontWeight: 'bold',
+  fontSize: 17,
+  color: '#BE5EAC'
+ },
+ financeReportContainer: {
+  flex: 1,
+  flexDirection: 'column',
+  width: 450,
+  height: 300,
+  backgroundColor: '#FFEBEB',
+  borderRadius: 15,
+  paddingHorizontal: 30,
+  paddingTop: 10,
+  paddingBottom: 15,
+ },
+ noData: {
+  fontFamily: 'Amethysta-Regular', 
+  fontSize: 16,
+  color: '#757575'
  }
 });
--- components/input-field.tsx
+++ components/input-field.tsx
@@ -1,18 +1,46 @@
 import React, { Dispatch, SetStateAction } from 'react';
-import { StyleProp, StyleSheet, Text, TextInput, TextStyle, View } from 'react-native';
+import { Image, Pressable, StyleProp, StyleSheet, Text, TextInput, TextStyle, View } from 'react-native';
 
 type InputFieldProps = {
   label?: string;
+  isButtonPresent?: boolean;
+  //this button is used to generate more expenses inside the scrollview
   placeholder: string;
   value: string;
   onChangeText: Dispatch<SetStateAction<string>>;
   style?: StyleProp<TextStyle>;
 };
 
-export const InputField = ({ label, placeholder, value, onChangeText, style, ...restProps}: InputFieldProps) => {
+const addNewExpense = ({placeholder, onChangeText, ...restProps}: InputFieldProps) => {
+  console.log('Add expense button was pressed.');
+  return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder={'Enter a dollar amount...'}
+          onChangeText={onChangeText}
+          {...restProps} 
+        />
+        <TextInput
+          style={styles.input}
+          placeholder={'Description...'}
+          onChangeText={onChangeText}
+          {...restProps} 
+        />
+      </View>
+  );
+};
+
+export const InputField = ({ label, isButtonPresent, placeholder, value, onChangeText, style, ...restProps}: InputFieldProps) => {
       return (
         <View style={styles.container}>
-          {label && <Text style={styles.label}>{label}</Text>}
+          <View style={styles.headerContainer}>
+            {label && <Text style={styles.label}>{label}</Text>}
+            {isButtonPresent && 
+              (<Pressable onPress={() => {addNewExpense({placeholder, value, onChangeText, ...restProps})}}>
+                <Image source={require('../assets/images/plus_button.svg')} style={styles.plusButtonContainer}/>
+              </Pressable>)} 
+          </View>
           <TextInput
             style={[styles.input, style]}
             placeholder={placeholder}
@@ -27,12 +55,20 @@ export const InputField = ({ label, placeholder, value, onChangeText, style, ...
   container: {
     marginBottom: 15,
   },
+  headerContainer: {
+    flexDirection: 'row',
+  },
   label: {
+    flex: 1,
     fontFamily: 'Amethysta-Regular',
     fontSize: 20,
     marginBottom: 8,
     color: '#7D28A4',
     fontWeight: '600'
+  },
+  plusButtonContainer: {
+    width: 25,
+    height: 25
   },
   input: {
     height: 40,
